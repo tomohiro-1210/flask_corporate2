36db9faa96236326791ce200394ef5a198c9e2ff
tomohiro_0225 <express22212270@gmail.com>
2024-08-16 15:16:22 +0900

    run.pyでflashの実装。 get_flashed_messages()を使ってサイトにflashで設定したメッセージを出力

--- flask_form2/run.py
+++ flask_form2/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 # フォーム関係
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -23,8 +23,12 @@ def input():
     # クラス手で定義したInputFormをインスタンス化
     form = InputForm()
     if form.validate_on_submit():
+        # sessionにフォームから入力したデータを引き渡し
         session['email'] = form.email.data
         session['username'] = form.username.data
+        # flashを利用して入力したことを知らせる
+        flash("入力したデータが表示されています。確認してください。")
+        # TOPページにリダイレクト
         return redirect(url_for('run_index'))
     return render_template('input.html', form=form)
 
