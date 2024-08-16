1d03666c5bf7d4c64e47263531d13f2a84cb5451
tomohiro_0225 <express22212270@gmail.com>
2024-08-16 13:54:49 +0900

    flash機能を使ってメッセージ実装 （from flask import flash, get_flashed_message()）

--- flask_form/app.py
+++ flask_form/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, session
+from flask import Flask, render_template, url_for, redirect, session, flash
 # flask_wtfでフォーム構築、wtformsでフォームのフィールドを一括管理
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
@@ -11,10 +11,10 @@ app.config['SECRET_KEY'] = 'flaskform'
 
 class RegistrationForm(FlaskForm):
     # フォームに表示する入力項目の設定
-    email = StringField('メールアドレス')
-    username = StringField('ユーザーネーム')
-    password = PasswordField('パスワード')
-    pw_config = PasswordField('パスワード(確認用)')
+    email = StringField('メールアドレス',validators=[DataRequired(), Email()])
+    username = StringField('ユーザーネーム', validators=[DataRequired()])
+    password = PasswordField('パスワード', validators=[DataRequired(), EqualTo('pw_config')])
+    pw_config = PasswordField('パスワード(確認用)', validators=[DataRequired()])
     submit = SubmitField('登録する')
 
 @app.route('/')
@@ -26,11 +26,15 @@ def index():
 def register():
     # フォームを使えるようにインスタンス化
     form = RegistrationForm()
-    # 入力したデータを保持する
+    # 入力したデータをチェックする
     if form.validate_on_submit():
+
+        # セッションにデータ格納
         session['email'] = form.email.data
         session['username'] = form.username.data
         session['password'] = form.password.data
+        # 登録後に表示するメッセージ
+        flash('ユーザーが登録されました')
         return redirect(url_for('user_maintenance'))
     return render_template('register.html', form=form)
 
