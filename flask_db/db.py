4c0462c18d9f625122d7cd7349f40b48e98e0e8a
tomohiro_0225 <express22212270@gmail.com>
2024-08-17 10:16:51 +0900

    DBのテーブル設定

--- flask_db/db.py
+++ flask_db/db.py
@@ -10,7 +10,25 @@ basedir = os.path.abspath(os.path.dirname(___file__))
 app.config['SQLARCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLARChEMY_TRACK_MODIFICATIONS'] = False #dbの変更履歴無効
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(app) #DBの生成？？
+
+# テーブルの定義
+class User(db.Model):
+    __tablename__ = 'users'
+
+    # dbのテーブルを定義
+    id = db.Column(db.Integer, primary_key=True) # Integerで数値型を指定する
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    passsword_hash = db.Column(db.String(128))
+
+    def __init__(self, email, username, password_hash):
+        self.email = email
+        self.username = username
+        self.passsword_hash = password_hash
+
+    def __repr__(self):
+        return f"Username: {self.username}"
 
 
 if __name__ == '__main__':
