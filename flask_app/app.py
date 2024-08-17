e1946f2b1bc9f821d3dd1c1766a15e00de20a6e0
tomohiro_0225 <express22212270@gmail.com>
2024-08-17 20:03:08 +0900

    company_blog>__init__.pyフォルダに移管作業 （import company_blogで__init__.pyのフォルダ内容を読み込める模様。）

--- flask_app/app.py
+++ flask_app/app.py
@@ -5,35 +5,8 @@ from wtforms import StringField, PasswordField, SubmitField, ValidationError
 # wtforms.validatorsでバリテーションチェック
 from wtforms.validators import DataRequired, Email, EqualTo
 # ログイン
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
-from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, logout_user, login_required, current_user
 
-# DBの読み込み
-import os 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-#タイムゾーン
-from datetime import datetime
-from pytz import timezone
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'flaskform'
-
-# dbの環境変数設定
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #dbの変更履歴無効
-
-# dbの作成
-db = SQLAlchemy(app) #DBの生成？？
-Migrate(app, db)
-
-# ログイン関係
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
 
 def localize_callback(*args, **kwargs):
     return 'このページにアクセスするには、ログインが必要です。'
@@ -50,74 +23,6 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(user_id)
-
-# Userモデル、テーブル
-class User(db.Model, UserMixin):
-    __tablename__ = 'users'
-
-    # dbのテーブルを定義
-    id = db.Column(db.Integer, primary_key=True) # Integerで数値型を指定する
-    email = db.Column(db.String(64), unique=True, index=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    passsword_hash = db.Column(db.String(128))
-    administrator = db.Column(db.String(1))
-    #リレーション設定
-    post = db.relationship('BlogPost', backref='author', lazy='dynamic')
-
-    def __init__(self, email, username, password, administrator):
-        self.email = email
-        self.username = username
-        self.passsword = password
-        self.administrator = administrator
-
-    def __repr__(self):
-        return f"Username: {self.username}"
-    
-    # パスワードのチェック
-    def check_password(self, password):
-        return check_password_hash(self.passsword_hash, password)
-    
-    @property
-    def password(self):
-        raise AttributeError('password is not a readable attribute')
-    
-    # フォームから受け取ったパスワードのハッシュ化処理をする。そして上のpassword_hashに戻っていくのか？
-    @password.setter
-    def password(self, password):
-        self.passsword_hash = generate_password_hash(password)
-
-    # 管理者か一般ユーザーかの判断
-    def is_administrator(self):
-        if self.administrator == "1":
-            return 1
-        else: 
-            return 0
-
-# ブログテーブル
-class BlogPost(db.Model):
-    __tablename__ = 'blog_post'
-
-    # テーブルの設定
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    date = db.Column(db.DateTime, default=datetime.now(timezone('Asia/tokyo')))
-    title = db.Column(db.String(140))
-    text = db.Column(db.Text)
-    summary = db.Column(db.String(140))
-    featured_image = db.Column(db.String(140))
-
-    def __init__(self, user_id, title, text, summary, featured_image):
-        self.title = title
-        self.text = text
-        self.featured_image = featured_image
-        self.user_id = user_id
-        self.summary = summary
-
-    def __repr__(self):
-        return f'postID: {self.id}, Title: {self.title}, Author: {self.author} \n'
 
 #ログインフォーム
 class LoginForm(FlaskForm):
---
+++ flask_app/company_blog/__init__.py
@@ -0,0 +1,26 @@
+# ログイン
+from flask_login import LoginManager
+
+# DBの読み込み
+import os 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# flask関係
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'flaskform'
+
+# dbの環境変数設定
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #dbの変更履歴無効
+
+# dbの作成
+db = SQLAlchemy(app) #DBの生成？？
+Migrate(app, db)
+
+# ログイン関係
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
