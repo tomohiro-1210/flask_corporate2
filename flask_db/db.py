69016839967709bb9bd75767d096412fcdbbb994
tomohiro_0225 <express22212270@gmail.com>
2024-08-17 10:10:15 +0900

    DBの定義

--- flask_db/db.py
+++ flask_db/db.py
@@ -8,6 +8,9 @@ app.config['SECRET_KEY'] = 'appdb'
 
 basedir = os.path.abspath(os.path.dirname(___file__))
 app.config['SQLARCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLARChEMY_TRACK_MODIFICATIONS'] = False #dbの変更履歴無効
+
+db = SQLAlchemy(app)
 
 
 if __name__ == '__main__':
