60ef1624cbc6aae53fee92d987f172da01d73a09
tomohiro_0225 <express22212270@gmail.com>
2024-08-17 11:08:41 +0900

    ブログテーブルの作成【BlogPost】 外部キー制約設定(usersテーブルのidとblog_postテーブルのuser_idを紐づけ)

--- flask_db/app.py
+++ flask_db/app.py
@@ -1,7 +1,12 @@
+from enum import unique
 import os 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+#タイムゾーン
+from datetime import datetime
+from pytz import timezone
 
 app = Flask(__name__)
 
@@ -34,6 +39,28 @@ class User(db.Model):
     def __repr__(self):
         return f"Username: {self.username}"
 
+# ブログテーブル
+class BlogPost(db.Model):
+    __tablename_ = 'blog_post'
+
+    # テーブルの設定
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    date = db.Column(db.DateTime, default=datetime.now(timezone('Asia/tokyo')))
+    title = db.Column(db.String(140))
+    text = db.Column(db.Text)
+    summary = db.Column(db.String(140))
+    featured_image = db.Column(db.String(140))
+
+    def __init__(self, user_id, title, text, summary, featured_image):
+        self.title = title
+        self.text = text
+        self.featured_image = featured_image
+        self.user_id = user_id
+        self.summary = summary
+
+    def __repr__(self):
+        return f'postID: {self.id}, Title: {self.title}'
 
 if __name__ == '__main__':
     app.run(debug=True)
