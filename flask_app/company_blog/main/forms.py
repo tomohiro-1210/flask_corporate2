43e34dc8217041a6ef9e1ba0eeb135f2c872b677
tomohiro_0225 <express22212270@gmail.com>
2024-08-18 12:28:27 +0900

    カテゴリー更新の関数作成(forms.py?UpdateCategoryForm, ) カテゴリー更新のテンプレート作成(blog_category.html) カテゴリー更新の削除関数作成(views.py>delete_category)

--- flask_app/company_blog/main/forms.py
+++ flask_app/company_blog/main/forms.py
@@ -12,3 +12,16 @@ class BlogCategoryForm(FlaskForm):
     def validate_category(self, field):
         if BlogCategory.query.filter_by(category=field.data).first():
             raise ValidationError('入力されたカテゴリーは既に使われています。')
+        
+# カテゴリーフォーム
+class UpdateCategoryForm(FlaskForm):
+    category  = StringField('カテゴリー名', validators=[DataRequired()])
+    submit = SubmitField('更新')
+
+    def __init__(self, blog_category_id, *args, **kwargs):
+        super(UpdateCategoryForm, self).__init__(*args, **kwargs)
+        self.id = blog_category_id
+
+    def validate_category(self, field):
+        if BlogCategory.query.filter_by(category=field.data).first():
+            raise ValidationError('入力されたカテゴリー名は既に使われております。')
--- flask_app/company_blog/main/views.py
+++ flask_app/company_blog/main/views.py
@@ -1,11 +1,12 @@
-from flask import Blueprint, render_template ,request, url_for, redirect, flash
-from flask_login import login_required
+from flask import Blueprint, render_template ,request, url_for, redirect, flash, abort
+from flask_login import login_required, current_user
 from company_blog.models import BlogCategory
-from company_blog.main.forms import BlogCategoryForm
+from company_blog.main.forms import BlogCategoryForm, UpdateCategoryForm
 from company_blog import db
 
 main = Blueprint('main', __name__)
 
+# カテゴリー一覧
 @main.route('/category_maintenance', methods=['GET', 'POST'])
 @login_required
 def category_maintenance():
@@ -23,5 +24,40 @@ def category_maintenance():
     elif form.errors:
         form.category.data = ''
         flash(form.errors['category'][0])
-
     return render_template('category_maintenance.html', blog_categories=blog_categories, form=form)
+
+# カテゴリー更新
+@main.route('/<int:blog_category_id>/blog_category', methods=['GET', 'POST'])
+@login_required
+def blog_category(blog_category_id):
+    # 管理者でないとき
+    if not current_user.is_administrator():
+        abort(403)
+    blog_category = BlogCategory.query.get_or_404(blog_category_id)
+    form = UpdateCategoryForm(blog_category_id)
+
+    # 間違ったときの処理
+    if form.validate_on_submit():
+        blog_category.category = form.category.data
+        db.session.commit()
+        flash('ブログカテゴリーが更新されました。')
+        return redirect(url_for('main.category_maintenance'))
+
+    elif request.method == 'GET':
+        form.category.data = blog_category.category
+    return render_template('blog_category.html', form=form)
+
+# カテゴリー削除
+@main.route('/<int:blog_category_id>/delete_category', methods=['GET', 'POST'])
+@login_required
+def delete_category(blog_category_id):
+    # 管理者でないとき
+    if not current_user.is_administrator():
+        abort(403)
+
+    # カテゴリーの削除処理
+    blog_category = BlogCategory.query.get_or_404(blog_category_id)
+    db.session.delete(blog_category)
+    db.session.commit()
+    flash('ブログカテゴリーが削除されました')
+    return redirect(url_for('main.category_maintenance'))
